17624def31f9b132e8c2ad8db802a5a418b729bf
天逸 <albert.xht@alibaba-inc.com>
2020-07-13 11:40:19 +0800

    fix

--- tensorflow/ops.py
+++ tensorflow/ops.py
@@ -82,8 +82,9 @@ def embedding_lookup(x, n_embed, d_embed, initializer, lookup_table=None,
       one_hot_idx = tf.one_hot(x, n_embed, dtype=dtype)
       einsum_prefix = get_einsum_prefix(x.shape.ndims)
       einsum_str = "{0}n,nd->{0}d".format(einsum_prefix)
-      print(one_hot_idx.get_shape(), lookup_table.get_shape(), "==embedding shape==")
       output = tf.einsum(einsum_str, one_hot_idx, lookup_table)
+      print(one_hot_idx.get_shape(), lookup_table.get_shape(), 
+        "==embedding shape==", output.get_shape())
     else:
       output = tf.nn.embedding_lookup(lookup_table, x)
 
@@ -114,10 +115,10 @@ def dense(x, out_shape, initializer, inp_shape=None, begin_axis=-1,
                              dtype=x.dtype,
                              initializer=initializer)
 
-    print(x.get_shape(), kernel.get_shape(), "==embedding shape==", prefix,
-        inp_str, out_str)
     output = tf.einsum(
         "{0}{1},{1}{2}->{0}{2}".format(prefix, inp_str, out_str), x, kernel)
+    print(x.get_shape(), kernel.get_shape(), "==embedding shape==", prefix,
+        inp_str, out_str, output.get_shape())
 
     if use_bias:
       bias = tf.get_variable("bias",
@@ -297,10 +298,11 @@ def rel_attn_core(
   # content based attention score
   r_w_bias = tf.get_variable("r_w_bias", [n_head, d_head],
                              dtype=tf_float, initializer=initializer)
-  print(q_head.get_shape(), (r_w_bias * scale).get_shape(), k_head.get_shape(), 
-    "==rel_attn_core shape==")
+  
   content_bias = tf.einsum("...ind,...jnd->...nij",
                            q_head + r_w_bias * scale, k_head)
+  print(q_head.get_shape(), (r_w_bias * scale).get_shape(), k_head.get_shape(), 
+    "==rel_attn_core shape==", content_bias.get_shape())
 
   # position based attention score
   if pos_enc is None:
@@ -353,8 +355,9 @@ def rel_attn_core(
   attn_prob = dropout_op(attn_prob, dropatt, training=is_training)
 
   # attention output
-  print(attn_prob.get_shape(), (v_head).get_shape(), "==attn_core shape==")
   attn_vec = tf.einsum("...nij,...jnd->...ind", attn_prob, v_head)
+  print(attn_prob.get_shape(), (v_head).get_shape(), 
+    "==attn_core shape==", attn_vec.get_shape())
 
   # things to monitor in attention
   ret_dict["content_bias"] = content_bias
@@ -457,10 +460,10 @@ def rel_pos_bias_gpu(q_head, pos_enc, d_model, n_head, d_head, klen,
                  initializer=initializer, scope="r", use_bias=False)
 
   # [B x T x N x D]
-  print((q_head + r_r_bias * scale).get_shape(), 
-    (r_head).get_shape(), "==rel_pos_bias_gpu shape==")
   pos_bias = tf.einsum("...inh,jnh->...nij", q_head + r_r_bias * scale,
                        r_head)
+  print((q_head + r_r_bias * scale).get_shape(), 
+    (r_head).get_shape(), "==rel_pos_bias_gpu shape==", pos_bias.get_shape())
   pos_bias = rel_shift(pos_bias, -2, klen, shift)
 
   if func_mask is not None:
@@ -494,10 +497,12 @@ def rel_pos_bias(q_head, pos_enc, d_model, n_head, d_head, initializer,
   # [(B) x T x N x D]
   prefix_k = get_einsum_prefix(enc_k_1.shape.ndims - 2)
   einsum_str = "...ind,{0}jd->...nij".format(prefix_k)
-  print((q_head_r_1).get_shape(), 
-    (enc_k_1).get_shape(), "==rel_pos_bias shape==")
+  
   pos_bias = (tf.einsum(einsum_str, q_head_r_1, enc_k_1) +
               tf.einsum(einsum_str, q_head_r_2, enc_k_2))
+
+  print((q_head_r_1).get_shape(), 
+    (enc_k_1).get_shape(), "==rel_pos_bias shape==", pos_bias.get_shape())
 
   if func_mask is not None:
     pos_bias *= func_mask
@@ -524,9 +529,10 @@ def rel_seg_bias(q_head, seg_mat, n_head, d_head, initializer, func_mask=None,
   scale = tf.cast(1.0 / np.sqrt(d_head), dtype)
   q_head_s = q_head + r_s_bias * scale
   # [... x N x T x 2]
-  print((q_head_s).get_shape(), 
-    (seg_embed).get_shape(), "==rel_seg_bias shape==")
+  
   seg_biases = tf.einsum("...inh,snh->...nis", q_head_s, seg_embed)
+  print((q_head_s).get_shape(), 
+    (seg_embed).get_shape(), "==rel_seg_bias shape==", seg_biases.get_shape())
 
   # Split into `diff` & `same`: [... x N x T x 1]
   seg_bias_diff, seg_bias_same = tf.split(seg_biases, 2, axis=-1)
@@ -572,12 +578,14 @@ def get_pos_enc(pos_id_q, pos_id_k, d_model, dropout, is_training,
   freq_seq = tf.cast(tf.range(0, d_model_half, 1.0), dtype=dtype)
   inv_freq = 1 / (10000 ** (freq_seq / d_model_half))
 
-  print((pos_id_q).get_shape(), 
-    (inv_freq).get_shape(), "==get_pos_enc shape==")
-  print((pos_id_k).get_shape(), 
-    (inv_freq).get_shape(), "==get_pos_enc shape==")
+  
   sinusoid_q = tf.einsum("...i,d->...id", pos_id_q, inv_freq)
   sinusoid_k = tf.einsum("...i,d->...id", pos_id_k, inv_freq)
+
+  print((pos_id_q).get_shape(), 
+    (inv_freq).get_shape(), "==get_pos_enc shape==", sinusoid_q.get_shape())
+  print((pos_id_k).get_shape(), 
+    (inv_freq).get_shape(), "==get_pos_enc shape==", sinusoid_k.get_shape())
 
   sin_enc_q = tf.sin(sinusoid_q)
   cos_enc_q = tf.cos(sinusoid_q)
@@ -607,9 +615,9 @@ def get_pos_enc_gpu(rel_pos_id, d_model, dropout, is_training,
   freq_seq = tf.cast(tf.range(0, d_model_half, 1.0), dtype=dtype)
   inv_freq = 1 / (10000 ** (freq_seq / d_model_half))
 
-  print((rel_pos_id).get_shape(), 
-    (inv_freq).get_shape(), "==get_pos_enc shape==")
   sinusoid = tf.einsum("...i,d->...id", rel_pos_id, inv_freq)
+  print((rel_pos_id).get_shape(), 
+    (inv_freq).get_shape(), "==get_pos_enc shape==", sinusoid.get_shape())
   sin_enc = tf.sin(sinusoid)
   cos_enc = tf.cos(sinusoid)
   sin_enc = dropout_op(sin_enc, dropout, training=is_training)
