87f65dd86500e2e946e34882e8130d8af9b48b58
天逸 <albert.xht@alibaba-inc.com>
2020-08-20 10:21:08 +0800

    add funnel-transformer-mlm-small

--- tensorflow/scripts/finetune_run_glue.sh
+++ tensorflow/scripts/finetune_run_glue.sh
@@ -7,12 +7,12 @@ TPU_NAME=albert1
 NUM_HOSTS=1
 NUM_CORE_PER_HOST=8
 
-
 GS_ROOT=gs://yyht_source/pretrain
-GS_INIT_CKPT_DIR=${GS_ROOT}/model/english/tiny/bert_small_span_mask_real_attn_net_config_tiny_enc_dec_no_skip_normal_ce
+GS_INIT_CKPT_DIR=${GS_ROOT}/model/english/tiny/bert_small_span_mask_real_attn_net_config_tiny_enc_dec_no_skip_denoise_mlm
 
 task=rte
-data_dir=${GS_ROOT}/glue/RTE
+root_data_dir=${GS_ROOT}/glue
+root_model_dir=${GS_ROOT}/proc_data/glue/mlm
 
 # task=sts-b
 # data_dir=${GS_ROOT}/data/glue/STS-B
@@ -23,34 +23,33 @@ data_dir=${GS_ROOT}/glue/RTE
 # task=mnli_matched
 # data_dir=${GS_ROOT}/data/glue/MNLI
 
-GS_PROC_DATA_DIR=${GS_ROOT}/proc_data/glue/enc_dec/${task}
-GS_MODEL_DIR=${GS_ROOT}/proc_data/glue/enc_dec/${task}
+# GS_PROC_DATA_DIR=${GS_ROOT}/proc_data/glue/mlm/${task}
+# GS_MODEL_DIR=${GS_ROOT}/proc_data/glue/mlm/${task}
 
 uncased=True
 tokenizer_type=word_piece
 tokenizer_path=${GS_INIT_CKPT_DIR}/vocab.uncased.txt
-init_checkpoint=${GS_INIT_CKPT_DIR}/model.ckpt-142000
-model_config=${GS_INIT_CKPT_DIR}/net_config_small_chinese_enc_dec_no_skip_uncased_english.json
+init_checkpoint=${GS_INIT_CKPT_DIR}/model.ckpt-1145800
+model_config=${GS_INIT_CKPT_DIR}/net_config_small_chinese_enc_dec_no_skip_denoise_mlm_uncased_english.json
 
 bsz=16
 NUM_HOSTS=1
 NUM_CORE_PER_HOST=1
 
 
-nohup python classifier.py \
+nohup python classifier_all_datasets.py \
     --use_tpu=True \
     --tpu=${TPU_NAME} \
-    --data_dir=${data_dir} \
-    --output_dir=${GS_PROC_DATA_DIR} \
+    --root_data_dir=${root_data_dir} \
     --uncased=${uncased} \
     --num_hosts=${NUM_HOSTS} \
     --num_core_per_host=${NUM_CORE_PER_HOST} \
     --tokenizer_type=${tokenizer_type} \
     --tokenizer_path=${tokenizer_path} \
-    --model_dir=${GS_MODEL_DIR} \
+    --root_model_dir=${root_model_dir} \
     --init_checkpoint=${init_checkpoint} \
     --model_config=${model_config} \
-    --learning_rate=0.00002 \
+    --learning_rate=0.0001 \
     --warmup_steps=155 \
     --train_steps=1550 \
     --train_batch_size=${bsz} \
